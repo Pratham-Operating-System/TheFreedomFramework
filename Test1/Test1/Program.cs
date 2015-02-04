2467905daa112a54b7e69dce7f629b0220e5864a
root <root@localhost.localdomain>
2015-02-04 11:46:43 +0530

    zxcaa

---
+++ Test1/Test1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace Test1
+{
+	[ServiceContract]
+	public class foo
+	{
+	}
+	class MainClass
+	{
+		
+		public static void Main (string[] args)
+		{
+			//Console.WriteLine ("Hello World!");
+			string ss = Downc().Result;
+			Console.WriteLine (ss);
+			//Task t 
+			List<string> s = new List<string>();
+
+			var dd = from aa in s
+			         where aa.Length > 1
+			         select aa;
+			
+		}
+		public static async Task<string> Downc()
+		{
+			using (HttpClient hc = new HttpClient ()) {
+				string ss = await hc.GetStringAsync ("http://www.prathamos.org");
+				return ss;
+			}
+		}
+	}
+}
